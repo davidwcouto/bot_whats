788e71e7d75f9d537f04ea120cc0df22d9a1f9f2
davidwcouto <david.kouto@hotmail.com>
2025-04-02 23:14:05 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -248,7 +248,6 @@ client.on("message", async (message) => {
 	
 	if (msg === "1" || msg === "2") {
 		clientesAtendidos.add(chatId); // Marca o cliente como atendidooo
-	} else {
 		if (!clientesAtendidos.has(chatId)) {
 		try {
 		await client.sendMessage(
