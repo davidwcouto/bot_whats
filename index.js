6c331d604cd3160e1f52e64b3b885945e0d5108a
davidwcouto <david.kouto@hotmail.com>
2025-04-06 21:57:22 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ const removerSilencedChats = (chatId) => {
 const buscarPreco = (produto) => {
     if (!produto) return "⚠ Nenhum produto foi informado. Digite o nome corretamente.";
 
-    // Se a mensagem for apenas "tela", "incell", "original" ou "nacional", retorna errod
+    // Se a mensagem for apenas "tela", "incell", "original" ou "nacional", retorna erro
     const termosInvalidos = ["preta", "tela", "incell", "incel", "original", "orig", "nacional", "nac"];
     const preposicoes = ["do", "da", "de"];
 
