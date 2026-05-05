3b3e2148b748ff5540eccbc863554a6f58b42ba8
davidwcouto <david.kouto@hotmail.com>
2026-05-05 15:08:05 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// --- Carregar contatos autorizados a partir do arquivo de texto ---
+// --- Carregar contatos autorizados a partir do arquivo de textoo ---
 let allowedContacts = [];
 try {
   const contactsData = fs.readFileSync("allowed.txt", "utf8");
