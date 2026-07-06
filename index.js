b280566e252d7b919b2b8292511630c531ff1e9b
davidwcouto <david.kouto@hotmail.com>
2026-07-06 14:38:26 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ const client = new Client({
 	authStrategy: new LocalAuth(),
 	  puppeteer: {
 		headless: true,
-		executablePath: "/nix/store/*/bin/chromium",
 		args: [
 		  "--no-sandbox",
 		  "--disable-setuid-sandbox",
