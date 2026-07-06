cac8c5726ed0457932542bfd355cc50cccccd050
davidwcouto <david.kouto@hotmail.com>
2026-07-06 14:10:20 -0300

    Atualizando o bot

--- index - Copia.js
+++ index - Copia.js
@@ -67,6 +67,7 @@ const client = new Client({
 	authStrategy: new LocalAuth(),
 		puppeteer:{
 		headless:true,
+		executablePath:process.env.PUPPETEER_EXECUTABLE_PATH,
 			args:[
 				'--no-sandbox',
 				'--disable-setuid-sandbox'
