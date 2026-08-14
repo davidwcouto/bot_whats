fe389884f91eb5d6494b659b2f7517a328fc3329
davidwcouto <david.kouto@hotmail.com>
2026-08-14 00:31:54 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -213,9 +213,10 @@ function prepararPesquisa(texto) {
 	pesquisa = pesquisa.replace(/\bnacional\b/g, "nac");
 
     // Placa de carga
-	pesquisa = pesquisa.replace(/\bdock\b/g, "placa de carga");
-	pesquisa = pesquisa.replace(/\bdock iphone\b/g, "flex de carga");
+	
 	pesquisa = pesquisa.replace(/\bdock de carga iphone\b/g, "flex de carga");	
+	pesquisa = pesquisa.replace(/\bdock iphone\b/g, "flex de carga");
+	pesquisa = pesquisa.replace(/\bdock\b/g, "placa de carga");
 
     // Se não informou o tipo da peça, assume tela
     const tipos = [
