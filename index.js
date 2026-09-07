4106b95f9f470de28acc6f53a28f4fee76c84f61
davidwcouto <david.kouto@hotmail.com>
2026-09-07 19:39:12 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -4893,6 +4893,7 @@ app.get(
 					<div style="
 						margin-bottom: 6px;
 						color: #f1c40f;
+						font-size: 10px;
 						white-space: pre-wrap;
 						overflow-wrap: anywhere;
 					">${escaparHtml(coleta.coletar)}</div>
